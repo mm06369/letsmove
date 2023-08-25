97f959676bfcbd90132d50df9819efed586258c6
muhammadqsolutions <muhammadqsolutions@gmail.com>
2023-08-25 16:38:46 +0500

    remove warnings

--- lib/components/bmi_widget.dart
+++ lib/components/bmi_widget.dart
@@ -8,23 +8,23 @@ class BMIWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-                  margin: EdgeInsets.all(10.0),
-                  padding: EdgeInsets.all(20),
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    color: Color.fromRGBO(241, 227, 255, 1.0),
+                    color: const Color.fromRGBO(241, 227, 255, 1.0),
                     borderRadius: BorderRadius.circular(15.0),
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
+                      const Text(
                         'Your BMI: ',
                         style: TextStyle(
                           color: Color.fromRGBO(190, 130, 255, 1.0),
                           fontSize: 18.0,
                         ),
                       ),
-                     Text("1.2")
+                     const Text("1.2")
                     ],
                   ),
                 
